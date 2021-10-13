80ba5af9c21131d8119fbaf6df959839f015045a
unknown <juanmhernandezr3@gmail.com>
2021-10-12 19:50:37 -0500

    first commit

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,20 +1,15 @@
 package runners;
+
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "src/test/resources/functionalTests",
-		glue= {"stepDefinitions"}, 
-		plugin = {
-				"pretty", 
-				//"html:target/cucumber-htmlreport", 
-				//"json:target/cucumber-report.json",	
-				"html:cucumber-htmlreport", 
-				"json:cucumber-report.json",
-				"com.cucumber.listener.ExtentCucumberFormatter:target/ExtendReport.html"}, tags= {"@all"}
-		)
+@CucumberOptions(features = "src/test/resources/functionalTests", glue = { "stepDefinitions" }, plugin = { "pretty",
+		"html:target/cucumber-htmlreport", "json:target/cucumber-report.json",
+		// "html:cucumber-htmlreport",
+		// "json:cucumber-report.json",
+		"com.cucumber.listener.ExtentCucumberFormatter:target/ExtendReport.html" }, tags = { "@all" })
 
 public class TestRunner {
 
